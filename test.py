f0f4aaba81017adaf5067ad061cebaad1ec2a53d
Aida Piccato <aidapiccato@gmail.com>
2021-10-04 18:07:55 -0400

    Removed test file

--- test.py
+++
@@ -1,7 +0,0 @@
-from search_world.envs import hallway
-
-if __name__ == '__main__':
-    hallway_env = hallway.Hallway()
-    hallway_env.reset()
-    action = [1, 0]
-    print(hallway_env.step(action))
